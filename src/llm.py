eaa3f50efa450032affccd5574e9d2d6a3a5bbb7
Плащинский Николай Дмитриевич <niccneimi@gmail.com>
2026-08-15 18:35:56 +0300

    feat: удержание контекста в рамках одной сессии

--- src/llm.py
+++ src/llm.py
@@ -2,7 +2,13 @@ from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from src.config import LLMOPS_API_KEY, LLMOPS_BASE_URL
 from src.retrieval import qdrant_tool
+from langgraph.checkpoint.memory import InMemorySaver
 import asyncio
+from dataclasses import dataclass
+
+@dataclass
+class Context:
+    user_id: str
 
 chat_model = ChatOpenAI(
     model = 'qwen3-coder-next',
@@ -13,6 +19,8 @@ chat_model = ChatOpenAI(
     streaming=True
 )
 
+checkpointer = InMemorySaver()
+
 agent = create_agent(
     model = chat_model,
     tools = [qdrant_tool],
@@ -22,23 +30,29 @@ agent = create_agent(
         "Если данных нет — ответь: 'данных нет'. "
         "Всегда указывай ссылки на Confluence-страницы по каждому факту. "
         "Дополняй запрос пользователя ключевыми словами для точного поиска."
-    )
+    ),
+    checkpointer=checkpointer
 )
 
 async def main():
-    content = """Что такое UMS"""
-
-    with open('llm_answer.md', 'w') as file:
-        agent_stream = agent.astream_events({
-            "messages": [{'role': 'user', 'content': content}]
-        })
-        async for event in agent_stream:
-            if event["event"] == "on_chat_model_stream":
-                chunk = event["data"]["chunk"]
-                if chunk.content:
-                    # print(chunk.content, end="", flush=True)
-                    file.write(chunk.content)
-                    file.flush()
+    config = {'configurable': {'thread_id': 1}}
+    content = ''
+    while content!='STOP':
+        content = input("\nВведите запрос: ")
+        if content == 'STOP':
+            break
+        with open('llm_answer.md', 'w') as file:
+            agent_stream = agent.astream_events(
+                {"messages": [{'role': 'user', 'content': content}]},
+                config=config,
+                context=Context(user_id='ndplaschin')
+                )
+            async for event in agent_stream:
+                if event["event"] == "on_chat_model_stream":
+                    chunk = event["data"]["chunk"]
+                    if chunk.content:
+                        file.write(chunk.content)
+                        file.flush()
 
 if __name__ == '__main__':
     asyncio.run(main())
